08d38759325ac6bc584c7e4a0081c933f47f7ec6
Arnaud gueras <arnaudgs@gmail.com>
2019-03-14 22:52:09 +0100

    get trello board info directly online

--- tools/calculate-storypoints.js
+++ tools/calculate-storypoints.js
@@ -1,39 +1,83 @@
 const exported = require("./exported.json");
+const fs = require("fs");
 
-let sum = exported.cards
-  .filter(
-    card =>
-      card.pluginData &&
-      card.pluginData.length &&
-      card.pluginData[0].idPlugin === "59d4ef8cfea15a55b0086614"
-  )
-  .map(card => JSON.parse(card.pluginData[0].value).points)
-  .reduce((accumulator, currentValue) => accumulator + currentValue);
+let request = require("request");
+const { boardId, key, token } = JSON.parse(
+  fs.readFileSync("../.trello-settings")
+);
 
-//minutes to hour (and days) converter
-function convertMinutes(num) {
-  let d = Math.floor(num / 1440); // 60*24
-  let h = Math.floor((num - d * 1440) / 60);
-  let m = Math.round(num % 60);
+async function getCards() {
+  var options = {
+    method: "GET",
+    url: `https://api.trello.com/1/boards/${boardId}`,
+    qs: {
+      key: key,
+      token: token,
+      actions: "none",
+      boardStars: "none",
+      cards: "all",
+      card_pluginData: "true",
+      checklists: "none",
+      customFields: "false",
+      fields:
+        "name,desc,descData,closed,idOrganization,pinned,url,shortUrl,prefs,labelNames",
+      lists: "open",
+      members: "none",
+      memberships: "none",
+      membersInvited: "none",
+      membersInvited_fields: "all",
+      pluginData: "false",
+      organization: "false",
+      organization_pluginData: "false",
+      myPrefs: "false",
+      tags: "false"
+    }
+  };
+  let cards = await new Promise((resolve, reject) => {
+    request(options, function(error, response, body) {
+      var json = JSON.parse(body);
+      resolve(json);
+    });
+  });
 
-  if (d > 0) {
-    return d + " days, " + h + " hours, " + m + " minutes";
-  } else {
-    return h + " hours, " + m + " minutes";
+  let sum = cards.cards
+    .filter(
+      card =>
+        card.pluginData &&
+        card.pluginData.length &&
+        card.pluginData[0].idPlugin === "59d4ef8cfea15a55b0086614"
+    )
+    .map(card => JSON.parse(card.pluginData[0].value).points)
+    .reduce((accumulator, currentValue) => accumulator + currentValue);
+
+  //minutes to hour (and days) converter
+  function convertMinutes(num) {
+    let d = Math.floor(num / 1440); // 60*24
+    let h = Math.floor((num - d * 1440) / 60);
+    let m = Math.round(num % 60);
+
+    if (d > 0) {
+      return d + " days, " + h + " hours, " + m + " minutes";
+    } else {
+      return h + " hours, " + m + " minutes";
+    }
   }
+
+  console.log("Total en heures : %s", sum);
+  let totalFormatted = convertMinutes(sum * 60);
+  console.log("Total : %s", totalFormatted);
+  let totalJourOuvres = convertMinutes(sum * 60 * (24 / 8));
+  console.log(
+    "Jours ouvrés : %s, à raison de 8h de travail par jour",
+    totalJourOuvres
+  );
+
+  let totalJourOuvresHorsWeekend = convertMinutes(
+    (sum * 60 * (24 / 8) * 7) / 5
+  );
+  console.log(
+    "Jours ouvrés : %s, à raison de 8h de travail par jour, et 5 jours par semaine",
+    totalJourOuvresHorsWeekend
+  );
 }
-
-console.log("Total en heures : %s", sum);
-let totalFormatted = convertMinutes(sum * 60);
-console.log("Total : %s", totalFormatted);
-let totalJourOuvres = convertMinutes(sum * 60 * (24 / 8));
-console.log(
-  "Jours ouvrés : %s, à raison de 8h de travail par jour",
-  totalJourOuvres
-);
-
-let totalJourOuvresHorsWeekend = convertMinutes((sum * 60 * (24 / 8) * 7) / 5);
-console.log(
-  "Jours ouvrés : %s, à raison de 8h de travail par jour, et 5 jours par semaine",
-  totalJourOuvresHorsWeekend
-);
+getCards();
