a6f359c15a5e67f09ef8edc810b9b2dbda223a69
vivo87 <32093453+vivo87@users.noreply.github.com>
2019-03-22 16:55:19 +0100

    Prettier lint staged (#4)
    * prettier
    
    * lint-staged
    
    * node sass

--- packages/shootizy-client/src/App.js
+++ packages/shootizy-client/src/App.js
@@ -1,25 +1,20 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component, useState } from "react";
+import logo from "./logo.svg";
+import "./App.scss";
+
+const TestComponent = () => {
+  const [count, setCount] = useState(0);
+
+  return <button onClick={() => setCount(count => count + 1)}>count = {count}</button>;
+};
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>Hello world</p>
+        <TestComponent />
       </div>
     );
   }
