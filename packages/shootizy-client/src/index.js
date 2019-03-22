92f25c9c09eb2b438e5231060533fb4473687e34
Arnaud gueras <arnaudgs@gmail.com>
2019-03-22 19:48:56 +0100

    reorganize client folder from viet organisation

--- packages/shootizy-client/src/index.js
+++ packages/shootizy-client/src/index.js
@@ -1,7 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import "./index.scss";
-import App from "./App";
+import "./styles/index.scss";
+import "./index.css";
+import App from "./scripts/App";
 import * as serviceWorker from "./serviceWorker";
 
 ReactDOM.render(<App />, document.getElementById("root"));
