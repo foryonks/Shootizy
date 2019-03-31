be56e86d43c3c1f5cfc4212558b695cc52b76f1e
Arnaud gueras <arnaudgs@gmail.com>
2019-03-31 21:53:42 +0200

    Add footer and finish

--- packages/shootizy-client/src/scripts/components/Footer/Footer.js
+++ packages/shootizy-client/src/scripts/components/Footer/Footer.js
@@ -5,23 +5,27 @@ import "./Footer.scss";
 
 const Footer = props => (
   <section className="Footer">
-    <h2 className="title">#Shootizy</h2>
-    <div className="description">
-      Rejoignez la communauté Shootizy sur tous <br /> vos réseaux sociaux préférés !
-    </div>
-    <div className="social">
-      <a href="/facebook" className="button-facebook">
-        <Icon name="facebook" />
-      </a>
-      <a href="/twitter" className="button-twitter">
-        <Icon name="twitter" />
-      </a>
-      <a href="/youtube" className="button-youtube">
-        Facebook
-      </a>
-      <a href="/facebook" className="button-facebook">
-        Facebook
-      </a>
+    <div className="footer-social-section">
+      <div className="footer-social-section-content">
+        <h2 className="title">#Shootizy</h2>
+        <div className="description">
+          Rejoignez la communauté Shootizy sur tous <br /> vos réseaux sociaux préférés !
+        </div>
+        <div className="social-buttons">
+          <a href="/facebook" className="button-facebook" title="Facebook">
+            <Icon name="facebook" />
+          </a>
+          <a href="/twitter" className="button-twitter" title="Twitter">
+            <Icon name="twitter" />
+          </a>
+          <a href="/youtube" className="button-youtube" title="Youtube">
+            <Icon name="youtube" />
+          </a>
+          <a href="/facebook" className="button-instagram" title="Instagram">
+            <Icon name="instagram" />
+          </a>
+        </div>
+      </div>
     </div>
   </section>
 );
