e3bc41a7579c185caa391e1d6147fbdd762a995a
Arnaud gueras <arnaudgs@gmail.com>
2019-04-02 23:27:09 +0200

    fix design carousel + logo

--- packages/shootizy-client/src/scripts/components/Carousel/Carousel.js
+++ packages/shootizy-client/src/scripts/components/Carousel/Carousel.js
@@ -52,13 +52,9 @@ const Carousel = props => (
         ))}
       </CarouselResponsive>
     </div>
-    <svg
-      className="carousel-mask"
-      data-name="Calque 1"
-      xmlns="http://www.w3.org/2000/svg"
-      viewBox="0 0 811 44">
+    <svg className="carousel-mask" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 813 44">
       <path
-        d="M0 0c97.95 23.5 245.72 37.41 411.21 37.41C571.4 37.41 715.12 22.24 813 0v44H0z"
+        d="M0 0c97.9 23.5 245.7 37.4 411.2 37.4C571.4 37.4 715.1 22.2 813 0v44H0V0z"
         fill="#fff"
       />
     </svg>
