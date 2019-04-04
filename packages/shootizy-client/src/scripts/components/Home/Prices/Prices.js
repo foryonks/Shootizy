9c0b59d6911d0fa4c1a9968b7b033790293e106c
Arnaud gueras <arnaudgs@gmail.com>
2019-04-04 18:34:55 +0200

    Prices pixel-perfect + small refacto

--- packages/shootizy-client/src/scripts/components/Home/Prices/Prices.js
+++ packages/shootizy-client/src/scripts/components/Home/Prices/Prices.js
@@ -3,12 +3,12 @@ import Interweave from "interweave";
 import PropTypes from "prop-types";
 
 const Prices = ({ contents, className }) => (
-  <div className={"Prices container-2 row row-3 " + className}>
+  <section className={"Prices container-2 row row-3 " + className}>
     {contents.map(({ qty, unit, of, html }, index) => (
       <div className="price-item card card-simple" key={index}>
         <span className="price-item-qty">
           {qty}
-          <em>{unit}</em>
+          <sup>{unit}</sup>
         </span>
         <span className="price-item-of">{of}</span>
         <p className="price-item-text">
@@ -16,7 +16,7 @@ const Prices = ({ contents, className }) => (
         </p>
       </div>
     ))}
-  </div>
+  </section>
 );
 
 Prices.propTypes = {
