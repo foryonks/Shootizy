86a881e2b0cd0658a3d880bf348dc5ec0f0f956e
Arnaud gueras <arnaudgs@gmail.com>
2019-04-09 11:06:30 +0200

    header sticky

--- packages/shootizy-client/src/scripts/components/Header/Header.js
+++ packages/shootizy-client/src/scripts/components/Header/Header.js
@@ -1,23 +1,44 @@
-import React from "react";
+import React, { useRef, useEffect, useState } from "react";
 import TopHeader from "./TopHeader";
 import NavBar from "./NavBar";
 import Logo from "./Logo";
+import useWindowScrollPosition from "scripts/hooks/useWindowScrollPosition";
 
 // import { Link } from "react-router-dom";
 
 // import logo from "@axa/web-toolkit/images/axa.svg";
+let sticky = 0;
+let gap = 0;
+const Header = (props, ref) => {
+  const headerRef = useRef();
+  const [className, setClassName] = useState();
+  let options = {
+    throttle: 40,
+  };
+  let { y } = useWindowScrollPosition(options);
 
-const Header = () => {
+  useEffect(() => {
+    const header = headerRef.current;
+    const content = window.getComputedStyle(header, ":before");
+    let value = content.getPropertyValue("content");
+    gap = gap === 0 ? parseInt(content.getPropertyValue("content").replace(/"/g, ""), 10) : gap;
+    sticky = sticky > 0 ? sticky : header.offsetTop - gap;
+    y > sticky ? setClassName("sticky") : setClassName("");
+    return () => {};
+  }, [y]);
+
   return (
-    <header className="header">
-      <TopHeader />
+    <header className={`header ${className}`}>
+      <div className="header-content">
+        <TopHeader />
 
-      <div className="header-main">
-        <Logo />
-        <span className="navigation">
-          <NavBar />
-          <button className="btn-big">Réserver mon Shooting</button>
-        </span>
+        <div className="header-main" ref={headerRef}>
+          <Logo />
+          <span className="navigation">
+            <NavBar />
+            <button className="btn-big">Réserver mon Shooting</button>
+          </span>
+        </div>
       </div>
     </header>
   );
---
+++ packages/shootizy-client/src/scripts/hooks/useWindowScrollPosition.js
@@ -0,0 +1,45 @@
+import { useState, useEffect } from "react";
+const _throttle = require("lodash.throttle");
+
+let supportsPassive = false;
+try {
+  var opts = Object.defineProperty({}, "passive", {
+    get: function() {
+      supportsPassive = true;
+      return true;
+    },
+  });
+  window.addEventListener("testPassive", null, opts);
+  window.removeEventListener("testPassive", null, opts);
+} catch (e) {}
+
+let getPosition = () => ({
+  x: window.pageXOffset,
+  y: window.pageYOffset,
+});
+
+let defaultOptions = {
+  throttle: 100,
+};
+
+function useWindowScrollPosition(options) {
+  let opts = Object.assign({}, defaultOptions, options);
+
+  let [position, setPosition] = useState(getPosition());
+
+  useEffect(() => {
+    let handleScroll = _throttle(() => {
+      setPosition(getPosition());
+    }, opts.throttle);
+
+    window.addEventListener("scroll", handleScroll, supportsPassive ? { passive: true } : false);
+
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
+
+  return position;
+}
+
+export default useWindowScrollPosition;
