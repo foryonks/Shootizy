ac31323a1b913993e10e324c912c8b9c58c2fc1d
Arnaud gueras <arnogues@users.noreply.github.com>
2019-04-19 09:49:33 +0200

    Feature/nav shootizy (#39)
    * nav-sub creation
    
    * creation sous-nav  end

--- packages/shootizy-client/src/scripts/components/Header/NavBar/NavBar.js
+++ packages/shootizy-client/src/scripts/components/Header/NavBar/NavBar.js
@@ -1,21 +1,35 @@
 import React from "react";
 import { NavLink } from "react-router-dom";
+import ThemesNavigation from "scripts/components/Product/ThemesNavigation/ThemesNavigation.js";
 
 const NAV_LINKS = [
   { path: "/comment-ca-marche", title: "Comment ça marche ?" },
-  { path: "/produit", title: "Shooting Studio" },
-  { path: "/shooting-sur-mesure", title: "Shooting sur mesure" },
+  {
+    path: "/produit",
+    title: "Shooting Studio",
+    subNav: (
+      <div className="sub-nav-content">
+        <h3>Choisissez le thème de votre Shooting</h3>
+        <ThemesNavigation showImage={true} />
+      </div>
+    ),
+  },
+  {
+    path: "/shooting-sur-mesure",
+    title: "Shooting sur mesure",
+  },
   { path: "/tarifs", title: "Tarifs" },
   { path: "/notre-book", title: "Notre book" },
 ];
 const NavBar = props => (
   <div className="nav-bar">
     <ul className="menu">
-      {NAV_LINKS.map(({ path, title }) => (
+      {NAV_LINKS.map(({ path, title, subNav }) => (
         <li key={path}>
           <NavLink to={path}>
             <span>{title}</span>
           </NavLink>
+          {subNav && <div className="nav-sub">{subNav}</div>}
         </li>
       ))}
     </ul>
