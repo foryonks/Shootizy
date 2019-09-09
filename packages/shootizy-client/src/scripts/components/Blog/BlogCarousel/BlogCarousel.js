aa8e7bbe6cd9da88f8454e6dc70ed1de42008231
Arnaud gueras <arnaudgs@gmail.com>
2019-09-09 08:50:32 +0200

    carousel is done ho yeah

--- packages/shootizy-client/src/scripts/components/Blog/BlogCarousel/BlogCarousel.js
+++ packages/shootizy-client/src/scripts/components/Blog/BlogCarousel/BlogCarousel.js
@@ -3,6 +3,7 @@ import useRemoteContents from "scripts/hooks/useRemoteContents";
 //import ArticleCard from "../ArticleCard";
 import Carousel from "scripts/components/_common/Carousel";
 import CarouselRenderer from "./CarouselRenderer";
+import "./BlogCarousel.scss";
 
 const BlogCarousel = ({ className }) => {
   let { contents: articles } = useRemoteContents("/api/blog/articles", []);
