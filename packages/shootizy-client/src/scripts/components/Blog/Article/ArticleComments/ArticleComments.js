4563eff8b182e75052198f74a9fd44c3619a8fbe
Arnaud gueras <arnaudgs@gmail.com>
2019-09-16 09:45:32 +0200

    add Pagination on comments

--- packages/shootizy-client/src/scripts/components/Blog/Article/ArticleComments/ArticleComments.js
+++ packages/shootizy-client/src/scripts/components/Blog/Article/ArticleComments/ArticleComments.js
@@ -4,27 +4,32 @@ import "./ArticleComments.scss";
 import useRemoteContents from "scripts/hooks/useRemoteContents";
 import CommentForm from "./CommentForm/CommentForm";
 import { formatDate } from "scripts/utils/DateUtils";
+import usePagination from "scripts/hooks/usePagination";
+const ITEMS_PER_PAGE = 10;
 
 const ArticleComments = ({ articleId }) => {
   const { contents: comments, load: reloadList } = useRemoteContents(
     `/api/blog/comments/article/${articleId}`,
     { initialState: [] }
   );
-  //if (!comments || !comments.length) return null;
+  const { getCurrentPage, PaginationComponent } = usePagination(comments || [], ITEMS_PER_PAGE);
   return (
     <div className="ArticleCommentsWrapper">
       <h3 className="Blog-block-title">Commentaires</h3>
       {comments && comments.length ? (
-        <ul className="comments">
-          {comments.map(({ commentId, author, date, comment }) => (
-            <li key={commentId}>
-              <div>
-                <strong>{author}</strong> / {formatDate(date)}
-              </div>
-              <div>{comment}</div>
-            </li>
-          ))}
-        </ul>
+        <div>
+          <ul className="comments">
+            {getCurrentPage().map(({ commentId, author, date, comment }) => (
+              <li key={commentId}>
+                <div>
+                  <strong>{author}</strong> / {formatDate(date)}
+                </div>
+                <div>{comment}</div>
+              </li>
+            ))}
+          </ul>
+          {PaginationComponent}
+        </div>
       ) : (
         "Aucun commentaire n'a encore été posté"
       )}
