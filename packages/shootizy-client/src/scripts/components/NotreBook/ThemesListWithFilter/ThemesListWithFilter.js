0e24372250bc317eb920e03b23a7ef5ec4b0ed3f
Arnaud gueras <arnaudgs@gmail.com>
2019-11-04 23:04:28 +0100

    fake for themes

--- packages/shootizy-client/src/scripts/components/NotreBook/ThemesListWithFilter/ThemesListWithFilter.js
+++ packages/shootizy-client/src/scripts/components/NotreBook/ThemesListWithFilter/ThemesListWithFilter.js
@@ -2,11 +2,14 @@ import React, { useContext } from "react";
 import { AppContext } from "scripts/contexts/App";
 import ThemesLister from "scripts/components/_common/ThemesLister";
 import "./ThemesListWithFilter.scss";
+import Gallery from "scripts/components/_common/Gallery";
+const { fakegallery } = Gallery;
 
 const ThemesListWithFilter = props => {
   const { state: appState } = useContext(AppContext);
   const list = appState.themes || [];
-  const themesArray = list.map(({ title, gallery }) => ({ key: title, value: gallery }));
+  //const themesArray = list.map(({ title, gallery }) => ({ key: title, value: gallery }));
+  const themesArray = list.map(({ title, gallery }) => ({ key: title, value: fakegallery(6) }));
   themesArray[0].selected = true;
 
   return (
