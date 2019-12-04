598a7c242e05c1785fc3d61c7cdec2a63477afc7
Arnaud gueras <arnaudgs@gmail.com>
2019-12-04 13:26:36 +0100

    fix errors with pakages and nodejs

--- backstop_data/engine_scripts/chromy/scrollToBottom.js
+++ backstop_data/engine_scripts/chromy/scrollToBottom.js
@@ -2,11 +2,7 @@ module.exports = function(chromy, scenario) {
   if (scenario.scrollToBottom) {
     for (var i = 0; i < 20; i++) {
       chromy.scroll(0, 1000).wait(100);
+      console.log(i);
     }
-
-    // .evaluate(`_element = '${element}'`)
-    // .evaluate(function() {
-    //   document.querySelector(_element).scrollIntoView();
-    // }, element);
   }
 };
