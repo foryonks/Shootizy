0c520caba2705cc0f7fc9b596b1fd5ca43d03f31
Arnaud gueras <arnaudgs@gmail.com>
2019-12-09 21:13:14 +0100

    save

--- packages/shootizy-client/src/scripts/components/Header/Header.js
+++ packages/shootizy-client/src/scripts/components/Header/Header.js
@@ -1,5 +1,6 @@
 import React, { useRef, useEffect, useState } from "react";
 import { Link } from "react-router-dom";
+import _throttle from "lodash.throttle";
 
 import Breadcrumbs from "scripts/components/Breadcrumbs";
 import TopHeader from "./TopHeader";
@@ -8,7 +9,7 @@ import Logo from "./Logo";
 import useWindowScrollPosition from "scripts/hooks/useWindowScrollPosition";
 
 let sticky = 0;
-let gap = 0;
+let gap = 33;
 
 const Header = (props, ref) => {
   const headerRef = useRef();
@@ -17,17 +18,33 @@ const Header = (props, ref) => {
     throttle: 25,
   };
   let { y } = useWindowScrollPosition(options);
+
+  // const updateGap = () => {
+  //   const header = headerRef.current;
+  //   const content = window.getComputedStyle(header, ":before");
+  //   gap = parseInt(content.getPropertyValue("content").replace(/"/g, ""), 10);
+  // };
+
   const updateHeaderSticky = () => {
-    const header = headerRef.current;
-    if (!gap) {
-      const content = window.getComputedStyle(header, ":before");
-      gap = parseInt(content.getPropertyValue("content").replace(/"/g, ""), 10);
-    }
-    sticky = sticky > 0 ? sticky : header.offsetTop - gap;
+    //const header = headerRef.current;
+    // if (!gap) {
+    //   updateGap();
+    // }
+    sticky = gap; //sticky > 0 ? sticky : header.offsetTop - gap;
     setClassName(y > sticky ? "sticky" : "");
   };
+
   useEffect(() => {
     updateHeaderSticky();
+    let timer;
+    const updateHeaderThrottle = _throttle(() => {
+      updateHeaderSticky();
+    }, 200);
+    document.addEventListener("resize", updateHeaderThrottle);
+    return () => {
+      document.removeEventListener("resize", updateHeaderThrottle);
+      clearTimeout(timer);
+    };
   });
 
   return (
