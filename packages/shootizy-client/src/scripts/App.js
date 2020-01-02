e4cedec6627613f4cf5c6ec8ff4c573cdaf70074
Arnaud gueras <arnaudgs@gmail.com>
2020-01-02 15:04:19 +0100

    fix IE

--- packages/shootizy-client/src/scripts/App.js
+++ packages/shootizy-client/src/scripts/App.js
@@ -1,7 +1,7 @@
 import React, { lazy, useEffect } from "react";
 import { Switch, withRouter } from "react-router-dom";
 import { Helmet } from "react-helmet";
-
+import "whatwg-fetch";
 import Debug from "./components/Debug";
 import LazyRoute from "./components/LazyRoute";
 
@@ -12,7 +12,16 @@ const AdminPage = lazy(() => import("scripts/pages/Admin"));
 
 const App = () => {
   useEffect(() => {
-    window.dispatchEvent(new Event("pageChange"));
+    if (typeof Event === "function") {
+      // modern browsers
+      window.dispatchEvent(new Event("pageChange"));
+    } else {
+      // for IE and other old browsers
+      // causes deprecation warning on modern browsers
+      var evt = window.document.createEvent("UIEvents");
+      evt.initUIEvent("pageChange", true, false, window, 0);
+      window.dispatchEvent(evt);
+    }
   });
   return (
     <div className="App">
