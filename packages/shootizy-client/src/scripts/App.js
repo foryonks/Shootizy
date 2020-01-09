dab366f23756fa9e72e95617f0357e719bd6baa6
Arnaud gueras <arnaudgs@gmail.com>
2020-01-09 15:18:06 +0100

    change imports lazy

--- packages/shootizy-client/src/scripts/App.js
+++ packages/shootizy-client/src/scripts/App.js
@@ -2,14 +2,16 @@ import React, { lazy, useEffect } from "react";
 import { Switch, withRouter } from "react-router-dom";
 import { Helmet } from "react-helmet";
 //import "whatwg-fetch";
-//import pf from "polyfill-array-includes";
+import "polyfill-array-includes";
 import Debug from "./components/Debug";
 import LazyRoute from "./components/LazyRoute";
 
 const DEFAULT_TITLE = "Shootizy";
 
-const PublicPage = lazy(() => import("scripts/pages/Public"));
-const AdminPage = lazy(() => import("scripts/pages/Admin"));
+// const PublicPage = lazy(() => import("scripts/pages/Public"));
+// const AdminPage = lazy(() => import("scripts/pages/Admin"));
+import PublicPage from "scripts/pages/Public";
+import AdminPage from "scripts/pages/Admin";
 
 const App = () => {
   useEffect(() => {
