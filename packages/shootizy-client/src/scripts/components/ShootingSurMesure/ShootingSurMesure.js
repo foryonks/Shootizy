c41958ba91d21ed69c85a4c73eb9fd5691152876
Arnaud gueras <arnaudgs@gmail.com>
2020-01-30 22:04:08 +0100

    shooting sur mesure fix

--- packages/shootizy-client/src/scripts/components/ShootingSurMesure/ShootingSurMesure.js
+++ packages/shootizy-client/src/scripts/components/ShootingSurMesure/ShootingSurMesure.js
@@ -9,13 +9,26 @@ import ThemesLister from "scripts/components/_common/ThemesLister";
 import VenirAuStudio from "scripts/components/Home/VenirAuStudio";
 import { AppContext } from "scripts/contexts/App";
 
+const entreprises = [
+  "/assets/design/surmesure/entreprises/orange.jpg",
+  "/assets/design/surmesure/entreprises/raja.jpg",
+  "/assets/design/surmesure/entreprises/renault.jpg",
+  "/assets/design/surmesure/entreprises/fleury-michon.jpg",
+  "/assets/design/surmesure/entreprises/lanvin.jpg",
+  "/assets/design/surmesure/entreprises/ministere-culture.jpg",
+  "/assets/design/surmesure/entreprises/m6.jpg",
+  "/assets/design/surmesure/entreprises/monceau-fleurs.jpg",
+  "/assets/design/surmesure/entreprises/accor.jpg",
+  "/assets/design/surmesure/entreprises/vinci.jpg",
+];
+
 const ShootingSurMesure = ({ className }) => {
   const { state: appState } = useContext(AppContext);
   const list = appState.surMesures;
 
   return (
     list && (
-      <div className={`ShootingSurMesureWrapper page-container`}>
+      <div className={`ShootingSurMesureWrapper page-section-grey-onmobile page-container`}>
         <Helmet bodyAttributes={{ class: "header-padding-page" }} />
         <HeaderImage
           src="/assets/design/headers/shooting-sur-mesure.svg"
@@ -23,9 +36,10 @@ const ShootingSurMesure = ({ className }) => {
           title="Décrivez-nous votre besoin <br>et obtenez un <strong>devis gratuit</strong> sur mesure !"
         />
         <FloatingBody />
-        <SurMesure fullForm={true} />
-
         <div className="container-2">
+          <SurMesure fullForm={true} />
+        </div>
+        <div className="container-2 ">
           {list.map(({ productId, title, description, image }, index) => (
             <div
               className={`mea-table ${index % 2 ? "mobile-column-reverse" : ""}`}
@@ -56,17 +70,25 @@ const ShootingSurMesure = ({ className }) => {
           ))}
         </div>
 
-        <div className="container-2">
+        <div className="container-2 ">
           <h2 className="title margin-title-surmesure">Quelques unes de nos réalisations</h2>
           <ThemesLister themesArray={list} />
-          <div className="clients">
-            <div>
-              <strong className="grey-text">Clients</strong>
-            </div>
-            <h2 className="title txt-l">Ils nous font confiance…</h2>
+        </div>
+        <div className="section-paddings">
+          <div className="container-2">
+            <div className="clients">
+              <div>
+                <strong className="grey-text">Clients</strong>
+              </div>
+              <h2 className="title txt-l">Ils nous font confiance…</h2>
 
-            <div className="full-image enterprises-confiance">
-              <img src="/assets/design/surmesure/entreprises.png" alt="" />
+              <ul className="enterprises-confiance images-inline-list">
+                {entreprises.map((img, key) => (
+                  <li key={key}>
+                    <img alt="" src={img} />
+                  </li>
+                ))}
+              </ul>
             </div>
           </div>
         </div>
